81acea73e33a0494009b19ed16f86c92b3130e0c
DaniloHY <nilo.11.dhy@gmail.com>
2022-12-01 22:07:26 -0300

    Modelo Filme, colocou em vetores, filmes na lista

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
     //Criar a base de dados para os filmes
 
     String titulo[] = {"Star Wars", "Cop Land", "Carros", "Ford x Ferrari", "Vingadores Ultimato", "Homem formiga e a Vespa"};
-    String subtitulo[] = {getString(R.string.sw),
-            getString(R.string.cop),
-            getString(R.string.carros),
-            getString(R.string.fxf),
-            getString(R.string.v_u),
-            getString(R.string.hfeav)};
+    String subtitulo[] = {"A queda de Darth Vader e do Império levou ao surgimento de uma nova força sombria: a Primeira Ordem. Eles procuram o jedi Luke Skywalker, desaparecido. A resistência tenta desesperadamente encontrá-lo antes para salvar a galáxia.",
+            "Um jovem policial de Nova Jersey, em uma atitude impensada, mata dois inocentes. Para protegê-lo, seus colegas forjam seu suicídio, mas o corregedor da polícia não acredita na história.",
+            "Ao viajar para a Califórnia, o famoso carro de corridas Relâmpago McQueen se perde e vai parar em Radiator Springs, uma cidadezinha na Rota 66. Ele conhece novos amigos e aprende lições que mudam sua forma de encarar a vida.",
+            "O projetista Carroll Shelby e o piloto Ken Miles enfrentaram a interferência empresarial, as leis da física e os próprios demônios para construir um carro de corrida para a Ford Motor derrotar a hegemonia de Enzo Ferrari nas 24 horas de Le Mans.",
+            "Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.",
+            "Scott Lang lida com as consequências de suas escolhas tanto como super-herói quanto como pai. Enquanto tenta reequilibrar sua vida com suas responsabilidades como o Homem-Formiga, ele é confrontado por Hope van Dyne e Dr. Hank Pym com uma nova missão urgente."};
     String rating[] = {"5", "4", "5", "5", "5", "4"};
-    int filmes[] = {R.drawable.Starwars, R.drawable.copland, R.drawable.carros, R.drawable.ford, R.drawable.vingadores, R.drawable.formiga};
+    int filmes[] = {R.drawable.starwars, R.drawable.copland, R.drawable.carros, R.drawable.ford, R.drawable.vingadores, R.drawable.formiga};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
