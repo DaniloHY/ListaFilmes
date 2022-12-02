6a6e2484e5691e3f0a78cc1444d8847322635cff
DaniloHY <nilo.11.dhy@gmail.com>
2022-12-01 21:53:03 -0300

    Modelo Filme, colocou em vetores

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -6,11 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listaFilmes;
+
+    //Criar a base de dados para os filmes
+
+    String titulo[] = {"Star Wars", "Cop Land", "Carros", "Ford x Ferrari", "Vingadores Ultimato", "Homem formiga e a Vespa"};
+    String subtitulo[] = {getString(R.string.sw),
+            getString(R.string.cop),
+            getString(R.string.carros),
+            getString(R.string.fxf),
+            getString(R.string.v_u),
+            getString(R.string.hfeav)};
+    String rating[] = {"5", "4", "5", "5", "5", "4"};
+    int filmes[] = {R.drawable.Starwars, R.drawable.copland, R.drawable.carros, R.drawable.ford, R.drawable.vingadores, R.drawable.formiga};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return filmes.length;
         }
 
         @Override
@@ -46,7 +60,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            //Variáveis do modelo
+            ImageView imagemFilme;
+            TextView tituloFilme, subtituloFilme, ratingFilme;
+
+            //Adaptador ligando ao modelo
+            View v = getLayoutInflater().inflate(R.layout.modelofilmes, null);
+
+            //Apresentar as variáveis do Java para o modelo XML
+            imagemFilme = v.findViewById(R.id.imgFilmes);
+            tituloFilme = v.findViewById(R.id.idTitulo);
+            subtituloFilme = v.findViewById(R.id.idSubtitulo);
+            ratingFilme = v.findViewById(R.id.idRating);
+
+            //Inserindo os valores nas variáveis do java
+
+            tituloFilme.setText(titulo[i]);
+            subtituloFilme.setText(subtitulo[i]);
+            ratingFilme.setText(rating[i]);
+            imagemFilme.setImageResource(filmes[i]);
+
+            return v;
         }
     }
 }
